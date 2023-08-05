d8405d81c25cc965d9303a24da62f840409f5391
mukulkumar-dev <90556544+mukulkumar-dev@users.noreply.github.com>
2023-08-05 21:39:23 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,257 @@
+<?php
+  include('./dbConnection.php');
+  // Header Include from mainInclude 
+  include('./mainInclude/header.php'); 
+?>  
+    <!-- Start Video Background-->
+    <div class="container-fluid remove-vid-marg">
+      <div class="vid-parent">
+        <video playsinline autoplay muted loop>
+          <source src="video/laptop-47311.mp4" />
+        </video>
+        <div class="vid-overlay"></div>
+      </div>
+      <div class="vid-content" >
+        <h1 class="my-content">Welcome to iSchool</h1>
+        <small class="my-content">Learn and Implement</small><br />
+        <?php    
+              if(!isset($_SESSION['is_login'])){
+                echo '<a class="btn btn-danger mt-3" href="#" data-toggle="modal" data-target="#stuRegModalCenter">Get Started</a>';
+              } else {
+                echo '<a class="btn btn-primary mt-3" href="student/studentProfile.php">My Profile</a>';
+              }
+          ?> 
+       
+      </div>
+            </div>
+    </div> <!-- End Video Background -->
+
+
+    
+    <div class="container-fluid bg-danger txt-banner"> <!-- Start Text Banner -->
+        <div class="row bottom-banner">
+          <div class="col-sm">
+            <h5> <i class="fas fa-book-open mr-3"></i> 100+ Online Courses</h5>
+          </div>
+          <div class="col-sm">
+            <h5><i class="fas fa-users mr-3"></i> 10+ Online Quizes</h5>
+          </div>
+          <div class="col-sm">
+            <h5><i class="fas fa-keyboard mr-3"></i>Study Material</h5>
+          </div>
+          <div class="col-sm">
+            <h5><i class="fas fa-rupee-sign mr-3"></i> Money Back Guarantee*</h5>
+          </div>
+        </div>
+    </div> <!-- End Text Banner -->
+    
+    <div class="container mt-5"> <!-- Start Most Popular Course -->
+      <h1 class="text-center">Popular Course</h1>
+      <div class="card-deck mt-4"> <!-- Start Most Popular Course 1st Card Deck -->
+        <?php
+        $sql = "SELECT * FROM course LIMIT 3";
+        $result = $conn->query($sql);
+        if($result->num_rows > 0){ 
+          while($row = $result->fetch_assoc()){
+            $course_id = $row['course_id'];
+            echo '
+            <a href="coursedetails.php?course_id='.$course_id.'" class="btn" style="text-align: left; padding:0px; margin:0px;">
+              <div class="card">
+                <img src="'.str_replace('..', '.', $row['course_img']).'" class="card-img-top" alt="Guitar" />
+                <div class="card-body">
+                  <h5 class="card-title">'.$row['course_name'].'</h5>
+                  <p class="card-text">'.$row['course_desc'].'</p>
+                </div>
+                <div class="card-footer">
+                  <p class="card-text d-inline">Price: <small><del>&#8377 '.$row['course_original_price'].'</del></small> 
+                  <span class="font-weight-bolder">&#8377 '.$row['course_price'].'<span></p> 
+                  <a class="btn btn-primary text-white font-weight-bolder 
+                  float-right" href="coursedetails.php?course_id='.$course_id.'">Enroll</a>
+                </div>
+              </div>
+            </a>  ';
+          }
+        }
+        ?>   
+      </div>  <!-- End Most Popular Course 1st Card Deck -->   
+      <div class="card-deck mt-4"> <!-- Start Most Popular Course 2nd Card Deck -->
+        <?php
+          $sql = "SELECT * FROM course LIMIT 3,3";
+          $result = $conn->query($sql);
+          if($result->num_rows > 0){ 
+            while($row = $result->fetch_assoc()){
+              $course_id = $row['course_id'];
+              echo '
+                <a href="coursedetails.php?course_id='.$course_id.'"  class="btn" style="text-align: left; padding:0px;">
+                  <div class="card">
+                    <img src="'.str_replace('..', '.', $row['course_img']).'" class="card-img-top" alt="Guitar" />
+                    <div class="card-body">
+                      <h5 class="card-title">'.$row['course_name'].'</h5>
+                      <p class="card-text">'.$row['course_desc'].'</p>
+                    </div>
+                    <div class="card-footer">
+                      <p class="card-text d-inline">Price: <small><del>&#8377 '.$row['course_original_price'].'</del></small> <span class="font-weight-bolder">&#8377 '.$row['course_price'].'<span></p> <a class="btn btn-primary text-white font-weight-bolder float-right" href="#">Enroll</a>
+                    </div>
+                  </div>
+                </a>  ';
+            }
+          }
+        ?>
+      </div>   <!-- End Most Popular Course 2nd Card Deck --> 
+      <div class="text-center m-2">
+        <a class="btn btn-danger btn-sm" href="courses.php">View All Course</a> 
+      </div>
+    </div>  <!-- End Most Popular Course -->
+
+
+    <div class="container-fluid bg-success txt-banner"> <br><br><br>
+    <section class="our-achievement style3 section overlay">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-12">
+                <div class="single-achievement wow fadeInUp" data-wow-delay=".2s">
+                    <h3 class="counter"><span id="secondo1" class="countup" cup-end="500">500</span>+</h3>
+                    <h4>Happy Students</h4>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-12">
+                <div class="single-achievement wow fadeInUp" data-wow-delay=".4s">
+                    <h3 class="counter"><span id="secondo2" class="countup" cup-end="70">70</span>+</h3>
+                    <h4>Online Study Material</h4>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-12">
+                <div class="single-achievement wow fadeInUp" data-wow-delay=".6s">
+                    <h3 class="counter"><span id="secondo3" class="countup" cup-end="100">100</span>%</h3>
+                    <h4>Satisfaction</h4>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-12">
+                <div class="single-achievement wow fadeInUp" data-wow-delay=".6s">
+                    <h3 class="counter"><span id="secondo3" class="countup" cup-end="100">100%</span>%</h3>
+                    <h4>Support</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</section> <br><br><br>
+        </div>
+
+
+
+    
+    <div class="container mt-5"> <!-- Start Most Popular Course -->
+      <h1 class="text-center">Some Study Material</h1>
+      <div class="card-deck mt-4"> <!-- Start Most Popular Course 1st Card Deck -->
+        <?php
+        $sql = "SELECT * FROM course LIMIT 3";
+        $result = $conn->query($sql);
+        if($result->num_rows > 0){ 
+          while($row = $result->fetch_assoc()){
+            $course_id = $row['course_id'];
+            echo '
+            <a href="coursedetails.php?course_id='.$course_id.'" class="btn" style="text-align: left; padding:0px; margin:0px;">
+              <div class="card">
+                <img src="'.str_replace('..', '.', $row['course_img']).'" class="card-img-top" alt="Guitar" />
+                <div class="card-body">
+                  <h5 class="card-title">'.$row['course_name'].'</h5>
+                  <p class="card-text">'.$row['course_desc'].'</p>
+                </div>
+                <div class="card-footer">
+                  <p class="card-text d-inline">Price: <small><del>&#8377 '.$row['course_original_price'].'</del></small> 
+                  <span class="font-weight-bolder">&#8377 '.$row['course_price'].'<span></p> 
+                  <a class="btn btn-primary text-white font-weight-bolder 
+                  float-right" href="coursedetails.php?course_id='.$course_id.'">Enroll</a>
+                </div>
+              </div>
+            </a>  ';
+          }
+        }
+        ?>   
+      </div>  <!-- End Most Popular Course 1st Card Deck -->  
+        <br> <br>
+
+
+    <?php 
+    // Contact Us
+    include('./contact.php'); 
+    ?>  
+
+     <!-- Start Students Testimonial -->
+      <div class="container-fluid mt-5" style="background-color: #4B7289" id="Feedback">
+        <h1 class="text-center testyheading p-4"> Student's Feedback </h1>
+        <div class="row">
+          <div class="col-md-12">
+            <div id="testimonial-slider" class="owl-carousel">
+            <?php 
+              $sql = "SELECT s.stu_name, s.stu_occ, s.stu_img, f.f_content FROM student AS s JOIN feedback AS f ON s.stu_id = f.stu_id";
+              $result = $conn->query($sql);
+              if($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()){
+                  $s_img = $row['stu_img'];
+                  $n_img = str_replace('../','',$s_img)
+            ?>
+              <div class="testimonial">
+                <p class="description">
+                <?php echo $row['f_content'];?>  
+                </p>
+                <div class="pic">
+                  <img src="<?php echo $n_img; ?>" alt=""/>
+                </div>
+                <div class="testimonial-prof">
+                  <h4><?php echo $row['stu_name']; ?></h4>
+                  <small><?php echo $row['stu_occ']; ?></small>
+                </div>
+              </div>
+              <?php }} ?>
+            </div>
+          </div>
+        </div>
+    </div>  <!-- End Students Testimonial -->
+
+    <div class="container-fluid bg-danger"> <!-- Start Social Follow -->
+        <div class="row text-white text-center p-1">
+          <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-facebook-f"></i> Facebook</a>
+          </div>
+          <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-twitter"></i> Twitter</a>
+          </div>
+          <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-whatsapp"></i> WhatsApp</a>
+          </div>
+          <div class="col-sm">
+            <a class="text-white social-hover" href="#"><i class="fab fa-instagram"></i> Instagram</a>
+          </div>
+        </div>
+    </div> <!-- End Social Follow -->
+
+    <!-- Start About Section -->
+    <div class="container-fluid p-4" style="background-color:#E9ECEF">
+      <div class="container" style="background-color:#E9ECEF">
+        <div class="row text-center">
+          <div class="col-sm">
+            <h5>About Us</h5>
+              <p> We Are Diploma Student, Try To Make A Simple Website Under The Guidance of S.K Singh And Nitin Kumar and We are thankful to them to give this oppertunity.</p>
+          </div>
+          <div class="col-sm">
+            <h5>Category</h5>
+            <a class="text-dark" href="#">Web Development</a><br />
+            <a class="text-dark" href="#">Web Designing</a><br />
+            <a class="text-dark" href="#">Android App Dev</a><br />
+            <a class="text-dark" href="#">Full Stack Developer</a><br />
+            <a class="text-dark" href="#">Flutter</a><br />
+          </div>
+          <div class="col-sm">
+            <h5>Contact Us</h5>
+            <p> Coder Group <br> Study In MMIT, <br> Aligarh<br> Ph.No.- 1234567890 </p>
+          </div>
+        </div>
+      </div>
+    </div> <!-- End About Section -->
+
+  <?php 
+    // Footer Include from mainInclude 
+    include('./mainInclude/footer.php'); 
+    
+  ?>  
